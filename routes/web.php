b4ed0ab349f5cac46b122fd53673adebfc2169e5
Amina Hamoud <amina123@tt.com>
2025-09-02 00:05:07 +0300

    "Resolve conflict in web.php by choosing the appropriate route"

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+use App\Http\Controllers\ShopController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [ShopController::class, 'index']);
+
