65152d46750819182e6558b2d3940324e2b523f6
Amina Hamoud <amina123@tt.com>
2025-09-04 02:39:10 +0300

    feat: Implement full e-commerce store with Blade layouts and partials

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stylish Store</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }} " alt="product-item">
+    <link rel="stylesheet" href="{{ asset('css/animate.css') }} " alt="product-item">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }} " alt="product-item">
+    <link rel="stylesheet" href="{{ asset('css/flexslider.css') }} " alt="product-item">
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }} " alt="product-item">
+    <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }} " alt="product-item">
+    <link rel="stylesheet" href="{{ asset('fonts/bootstrap/') }} " alt="product-item">
+    <link rel="stylesheet" href="{{ asset('fonts/icomoon/') }} " alt="product-item">
+    <!-- هنا ممكن تضيف أي ملفات CSS ثانية -->
+        <!-- JS -->
+        <script src="{{ asset('js.script.js') }}"></script>
+
+        <!-- الصور -->
+        <img src="{{ asset('images/card-item1.jpg') }}" alt="product-item">
+        <img src="{{ asset('asseat/card-item1.jpg') }}" alt="product-item">
+
+</head>
+<body>
+
+
+    @include('partials.header')
+
+    <main>
+        @yield('content')
+    </main>
+
+    @include('partials.footer')
+
+    <script src="{{ asset('js/jquery-1.11.0.min.js') }}"></script>
+    <script src="{{ asset('js/plugins.js') }}"></script>
+    <script src="{{ asset('js/script.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-
 use App\Http\Controllers\ProdactController;
 use App\Http\Controllers\ShopController;
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ServicesController;
-use App\Http\Controllers\contactController;
+// use Illuminate\Support\Facades\Route;
 
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\contactController;
+use App\Http\Controllers\ServicesController;
 
 
 // Route::get('/', function () {
@@ -20,7 +21,6 @@ use App\Http\Controllers\contactController;
 
 Route::get('/pages/about', [HomeController::class, 'index']);
 
-
 // Route::get('/pages/services', [ServicesController::class, 'index']);
 
 Route::get('/pages/prodact', [ProdactController::class, 'index']);
@@ -28,4 +28,8 @@ Route::get('/pages/prodact', [ProdactController::class, 'index']);
 Route::get('/', [contactController::class, 'index'])->name('about');
 // Route::get('/pages/contact', [contactController::class, 'index']);
 
-Route::get('/pages/single', [contactController::class, 'single'])->name('single.blade');
+// Route::get('/pages/single', [contactController::class, 'single'])->name('single.blade');
+Route::get('/pages/single', [ProdactController::class, 'single'])->name('single.blade');
+
+// Route::get('/', [ShopController::class, 'index']);
+
