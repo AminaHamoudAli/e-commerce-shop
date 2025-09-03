ebe35ab07ff372d3c27226a1c50737917b82386c
Amina Hamoud <amina123@tt.com>
2025-09-04 02:09:56 +0300

    feat: Integrate e-commerce template with Blade layout

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    //
     public function index() {
     return view('shop.welcome');
 }
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 
+use App\Http\Controllers\ProdactController;
 use App\Http\Controllers\ShopController;
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ServicesController;
+use App\Http\Controllers\contactController;
+
+
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::get('/', [ShopController::class, 'index']);
+// Route::get('/', [ShopController::class, 'index']);
 
+Route::get('/pages/about', [HomeController::class, 'index']);
+
+
+// Route::get('/pages/services', [ServicesController::class, 'index']);
+
+Route::get('/pages/prodact', [ProdactController::class, 'index']);
+
+Route::get('/', [contactController::class, 'index'])->name('about');
+// Route::get('/pages/contact', [contactController::class, 'index']);
+
+Route::get('/pages/single', [ProdactController::class, 'single'])->name('single.blade');
