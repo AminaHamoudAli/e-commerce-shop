67cfc129e0ccba4fad6418650911681603b88490
Amina Hamoud <amina123@tt.com>
2025-09-05 18:14:19 +0300

    feat: Implement Read (listing & showing single
    product) functionality

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,14 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ProdactController;
 use App\Http\Controllers\ShopController;
+use App\Http\Controllers\ProductsController;
 
 // use Illuminate\Support\Facades\Route;
 
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\contactController;
+use App\Http\Controllers\ProdactsController;
 use App\Http\Controllers\ServicesController;
 
 
@@ -17,19 +19,22 @@ use App\Http\Controllers\ServicesController;
 //     return view('welcome');
 // });
 
-// Route::get('/', [ShopController::class, 'index']);
+
 
 Route::get('/pages/about', [HomeController::class, 'index']);
 
-// Route::get('/pages/services', [ServicesController::class, 'index']);
+
 
 Route::get('/pages/prodact', [ProdactController::class, 'index']);
 
 Route::get('/', [contactController::class, 'index'])->name('about');
-// Route::get('/pages/contact', [contactController::class, 'index']);
+
 
 // Route::get('/pages/single', [contactController::class, 'single'])->name('single.blade');
 Route::get('/pages/single', [ProdactController::class, 'single'])->name('single.blade');
 
 // Route::get('/', [ShopController::class, 'index']);
 
+
+Route::get('/products', [ProductsController::class, 'index'])->name('products');
+Route::get('/products/{id}', [ProductsController::class, 'show'])->name('products');
