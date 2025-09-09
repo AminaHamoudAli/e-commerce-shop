b23f2b6f0d462a978c78317fd21e682c16fc8e00
Amina Hamoud <amina123@tt.com>
2025-09-10 02:13:23 +0300

    feat: Control UI components with policies

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -4,7 +4,7 @@
     <h1>Products</h1>
     <table border="1" cellpadding="5">
         <tr>
-            <th>ID</th><th>Name</th><th>Price</th><th>Category</th>
+            <th>ID</th><th>Name</th><th>Price</th><th>Category</th><th>Actions</th>
         </tr>
         @foreach ($products as $product)
             <tr>
@@ -12,6 +12,15 @@
                 <td>{{ $product->name }}</td>
                 <td>${{ $product->price }}</td>
                 <td>{{ $product->category->name ?? 'No Category' }}</td>
+                <td>
+                    @can('update', $product)
+                        <button>Edit Product</button>
+                    @endcan
+
+                    @can('delete', $product)
+                        <button>Delete Product</button>
+                    @endcan
+                </td>
             </tr>
         @endforeach
     </table>
