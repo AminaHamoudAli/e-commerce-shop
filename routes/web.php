503cfc28ababbdac4fabbaf708355d6a078eed3f
Amina Hamoud <amina123@tt.com>
2025-09-10 02:16:50 +0300

    feat: Apply middleware for route protection

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
+use App\Models\Product;
+use App\Models\Category;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +21,29 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::middleware(['auth', 'can:access-admin-panel'])->prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    });
+
+    Route::get('/products', function () {
+        $products = Product::with('category')->get();
+        return view('admin.products', compact('products'));
+    });
+
+    Route::get('/categories', function () {
+        $categories = Category::with('products')->get();
+        return view('admin.categories', compact('categories'));
+    });
+});
+
+
+
+// Route::prefix('admin')->middleware(['auth'])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('admin.dashboard');
+//     });
+//     // باقي الـ routes
+// });
